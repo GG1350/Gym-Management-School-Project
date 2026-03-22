9e7b5ffe1b1dc9f1fd52b143eedc597ec46d10a0
kokod <kokod@DESKTOP-4KGOLT1>
2026-03-22 17:54:21 +0200

    APP-Interface-Repos

---
+++ Gym Management [Project]/APP/IWorkoutRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management__Project_.APP
+{
+    internal interface IWorkoutRepository
+    {
+    }
+}

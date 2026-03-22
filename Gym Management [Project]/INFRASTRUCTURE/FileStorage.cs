13c315d9e2c15a29d4f45b8341441a37dad04abf
Георги Захариев <gocko1305@gmail.com>
2026-03-22 12:51:52 +0200

    DId a part of the repositories

--- Gym Management [Project]/INFRASTRUCTURE/FileStorage.cs
+++ Gym Management [Project]/INFRASTRUCTURE/FileStorage.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Gym_Management__Project_.INFRASTRUCTURE
 {
     public class FileStorage
     {
+        private readonly string _filePath;
 
+        public FileStorage(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public GymStorage Load()
+        {
+            if(!File.Exists(_filePath))
+            {
+                return new GymStorage();
+            }
+            var json = File.ReadAllText(_filePath);
+               
+            var storage = JsonSerializer.Deserialize<GymStorage>(json);
+            if(storage== null)
+            {
+                throw new Exception("Deserialization failed. The file content is not valid.");
+            }
+            return storage;
+        }
+        public void Save(GymStorage storage)
+        {
+            var json = JsonSerializer.Serialize(storage);
+            File.WriteAllText(_filePath, json);
+        }
     }
 }
---
+++ Gym Management [Project]/INFRASTRUCTURE/GymStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Gym_Management__Project_.DOMAIN.Entities;
+
+namespace Gym_Management__Project_.INFRASTRUCTURE
+{
+    public class GymStorage
+    {
+        public int NextId { get; set; } = 1;
+        public List<Trainers> Trainers { get; set; } = new List<Trainers>(); 
+
+        public List<Members> Members { get; set; } = new List<Members>();
+
+        public List<Workouts> Workouts { get; set; } = new List<Workouts>();
+    }
+}

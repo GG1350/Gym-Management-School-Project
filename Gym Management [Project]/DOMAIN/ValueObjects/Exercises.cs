fe36e9fe38925801e83c8dd14a51258a72f3eb54
Георги Захариев <gocko1305@gmail.com>
2026-03-22 12:22:17 +0200

    Exercises class should be OK and done

--- Gym Management [Project]/DOMAIN/ValueObjects/Exercises.cs
+++ Gym Management [Project]/DOMAIN/ValueObjects/Exercises.cs
@@ -8,5 +8,33 @@ namespace Gym_Management__Project_.DOMAIN.Entities
 {
     public class Exercises
     {
+        public string Name { get; set; } = string.Empty;
+        public int DurationMinutes { get; set; }
+        public double MET { get; set; }
+
+        public Exercises(string name, int durationMinutes, double met)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Exercise name is required");
+
+            if (durationMinutes <= 0)
+                throw new ArgumentException("Duration must be greater than 0");
+
+            if (met <= 0)
+                throw new ArgumentException("MET must be greater than 0");
+
+            Name = name;
+            DurationMinutes = durationMinutes;
+            MET = met;
+        }
+
+        public double CalculateCalories(double weightKg)
+        {
+            if (weightKg <= 0)
+                throw new ArgumentException("Weight must be greater than 0");
+
+            //double durationHours = DurationMinutes / 60.0;
+            return MET * weightKg * (DurationMinutes/60.0);
+        }
     }
 }

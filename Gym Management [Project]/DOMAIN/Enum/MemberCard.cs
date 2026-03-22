cc9c9b74e5635fb3bc4f90142da731bf67c52529
Георги Захариев <gocko1305@gmail.com>
2026-03-22 11:14:25 +0200

    Added enum "MemberCard" required for member class

---
+++ Gym Management [Project]/DOMAIN/Enum/MemberCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management__Project_.DOMAIN.Enum
+{
+    public enum MemberCard
+    {
+        Active,
+        Frozen,
+        Terminated,
+        Expired
+    }
+}

aa926500bdec0a9067285131781d8824a2524aa2
plamenivn <plamenivanov768@gmail.com>
2026-04-27 14:51:26 +0300

    ITrainersRepository ot Kosu

--- Gym Management [Project]/APP/ITrainersRepository.cs
+++ Gym Management [Project]/APP/ITrainersRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gym_Management__Project_.DOMAIN.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Gym_Management__Project_.APP
 {
     public interface ITrainersRepository
     {
+        Trainers GetById(int id);
+        void Save(Trainers trainers);
+        IReadOnlyList<Trainers> GetAll();
     }
 }

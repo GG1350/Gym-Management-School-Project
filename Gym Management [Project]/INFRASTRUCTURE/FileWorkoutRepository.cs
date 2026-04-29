fedd87e50b1e4330a3248750c79bda43b99a3268
plamenivn <plamenivanov768@gmail.com>
2026-04-29 11:27:06 +0300

    FileWorkoutRepostitory ot Kosu

--- Gym Management [Project]/INFRASTRUCTURE/FileWorkoutRepository.cs
+++ Gym Management [Project]/INFRASTRUCTURE/FileWorkoutRepository.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Gym_Management__Project_.DOMAIN.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gym_Management__Project_.APP;
 
 namespace Gym_Management__Project_.INFRASTRUCTURE
 {
-    internal class FileWorkoutRepository
+    public class FileWorkoutRepository : IWorkoutRepository
     {
+        private readonly FileStorage storage;
+
+        public FileWorkoutRepository(FileStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public IReadOnlyList<Workouts> GetAll()
+        {
+            var db = storage.Load();
+            return db.Workouts;
+        }
+
+        public void Save(Workouts Workouts)
+        {
+            var db = storage.Load();
+
+            if (Workouts.Id == 0)
+            {
+                bool found = true;
+                var newWorkouts = new Workouts(
+                    db.NextId++,
+                    Workouts.Type,
+                    Workouts.Exercises
+                    );
+                db.Workouts.Add(newWorkouts);
+            }
+            else
+            {
+                bool isFound = true;
+
+                for (int i = 0; i < db.Workouts.Count; i++)
+                {
+                    if (db.Workouts[i].Id == Workouts.Id)
+                    {
+                        db.Workouts[i] = Workouts;
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (!isFound)
+                {
+                    throw new Exception("Trainer not found");
+                }
+            }
+
+            storage.Save(db);
+        }
     }
 }

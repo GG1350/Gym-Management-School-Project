b6a89c791f5ab11c2caf7fb91dfcdca4005c3a37
plamenivn <plamenivanov768@gmail.com>
2026-04-29 11:41:40 +0300

    IWorkoutRepository ot Kosu

--- Gym Management [Project]/APP/IWorkoutRepository.cs
+++ Gym Management [Project]/APP/IWorkoutRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gym_Management__Project_.DOMAIN.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Gym_Management__Project_.APP
 {
     public interface IWorkoutRepository
     {
+        Workouts GetById(int id);
+        void Save(Workouts Workouts);
+        IReadOnlyList<Workouts> GetAll();
     }
 }

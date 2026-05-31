87308b54b78572081ce4ae4633527049fbfd1791
kokod <kokod@DESKTOP-4KGOLT1>
2026-05-31 17:53:44 +0300

    Some code added to EFRepos

--- Gym Management [Project]/INFRASTRUCTURE/EFRepositories/EFTrainersRepository.cs
+++ Gym Management [Project]/INFRASTRUCTURE/EFRepositories/EFTrainersRepository.cs
@@ -1,4 +1,6 @@
-﻿using Gym_Management__Project_.DOMAIN.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Gym_Management__Project_.APP;
+using Gym_Management__Project_.DOMAIN.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +29,26 @@ namespace Gym_Management__Project_.INFRASTRUCTURE.EFRepositories
 
             return trainers;
         }
+        public IReadOnlyList<Trainers> GetAll()
+        {
+            return context.Trainers
+                //.Include(m => m.Workouts)
+                .ToList();
+        }
 
+        public void Save(Trainers trainers)
+        {
+            if (trainers == null)
+                throw new ArgumentNullException(nameof(trainers));
+
+            context.Trainers.Add(trainers);
+            context.SaveChanges();
+        }
+
+        public void Update(int id, int wId, int mId)
+        {
+            //context.Trainers.Update(trainers); ДА СЕ ОПРАВИ
+            //context.SaveChanges();
+        }
     }
 }
--- Gym Management [Project]/INFRASTRUCTURE/EFRepositories/EFWorkoutRepository.cs
+++ Gym Management [Project]/INFRASTRUCTURE/EFRepositories/EFWorkoutRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Gym_Management__Project_.APP;
+using Gym_Management__Project_.DOMAIN.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace Gym_Management__Project_.INFRASTRUCTURE.EFRepositories
 {
-    public class EFWorkoutRepository
+    public class EFWorkoutRepository : IWorkoutRepository
     {
+        private readonly GymDbContext context;
+
+        public EFWorkoutRepository(GymDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Workouts GetById(int id)
+        {
+            var workouts = context.Workouts
+                //.Include(m => m.Workouts)
+                .FirstOrDefault(m => m.Id == id);
+
+            if (workouts == null)
+                throw new Exception("Workout not found.");
+
+            return workouts;
+        }
+
+        public IReadOnlyList<Workouts> GetAll()
+        {
+            return context.Workouts
+                //.Include(m => m.Workouts)
+                .ToList();
+        }
+
+        public void Save(Workouts workouts)
+        {
+            if (workouts == null)
+                throw new ArgumentNullException(nameof(workouts));
+
+            context.Workouts.Add(workouts);
+            context.SaveChanges();
+        }
     }
 }

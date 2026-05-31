06ced6b1b74b8d0158849c428791fc7da64b400b
kokod <kokod@DESKTOP-4KGOLT1>
2026-05-31 17:26:51 +0300

    EFRepo classes created + some function in EFMemberRepository

---
+++ Gym Management [Project]/INFRASTRUCTURE/EFRepositories/EFMemberRepo.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Gym_Management__Project_.APP;
+using Gym_Management__Project_.DOMAIN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management__Project_.INFRASTRUCTURE.EFRepositories
+{
+    public class EfMemberRepository : IMemberRepository
+    {
+        private readonly GymDbContext context;
+
+        public EfMemberRepository(GymDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Members GetById(int id)
+        {
+            var member = context.Members
+                //.Include(m => m.Workouts)
+                .FirstOrDefault(m => m.Id == id);
+
+            if (member == null)
+                throw new Exception("Member not found.");
+
+            return member;
+        }
+
+        public IReadOnlyList<Members> GetAll()
+        {
+            return context.Members
+                //.Include(m => m.Workouts)
+                .ToList();
+        }
+
+        public void Save(Members member)
+        {
+            if (member == null)
+                throw new ArgumentNullException(nameof(member));
+
+            context.Members.Add(member);
+            context.SaveChanges();
+        }
+
+        public void Update(int id, int wId)
+        {
+            //context.Members.Update(member); ДА СЕ ОПРАВИ
+            //context.SaveChanges();
+        }
+    }
+}
---
+++ Gym Management [Project]/INFRASTRUCTURE/EFRepositories/EFTrainersRepository.cs
@@ -0,0 +1,32 @@
+﻿using Gym_Management__Project_.DOMAIN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management__Project_.INFRASTRUCTURE.EFRepositories
+{
+    public class EFTrainersRepository : ITrainersRepository
+    {
+        private readonly GymDbContext context;
+
+        public EFTrainersRepository(GymDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Trainers GetById(int id)
+        {
+            var trainers = context.Trainers
+                //.Include(m => m.Workouts)
+                .FirstOrDefault(m => m.Id == id);
+
+            if (trainers == null)
+                throw new Exception("Trainer not found.");
+
+            return trainers;
+        }
+
+    }
+}
